ec129488e2c514622f24ef6188336feef239aefd
JulienTouchard <julien.touchard@free.fr>
2023-12-19 15:46:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Recette from './components/Recette/Recette'
+import MaNav from './components/MaNav/MaNav';
+function App() {
+  return (
+    <>
+    <MaNav></MaNav>
+    <Recette></Recette>
+    </>
+  );
+}
+
+export default App;
